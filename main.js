1e8fecb92bbd91587bb1d077176c8ed0d87f7e50
Mutebu <ms18.cgafv@gmail.com>
2025-06-09 05:01:18 +0400

    I made arrays with words that when combined make a message

---
+++ main.js
@@ -0,0 +1,3 @@
+const subjects = ["Believe", "Follow", "Embrace", "Trust", "Discover"];
+const actions = ["your dreams", "the journey", "your power", "the light", "your potential"];
+const results = ["to change the world.", "to find your truth.", "and rise above.", "to become unstoppable.", "and shine bright."];
