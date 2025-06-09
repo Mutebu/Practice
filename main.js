ed352480b6b91f11b85c65c6f373def9b8bdb0fb
Mutebu <ms18.cgafv@gmail.com>
2025-06-09 10:15:38 +0400

    add random feature and print it in a console

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 const subjects = ["Believe", "Follow", "Embrace", "Trust", "Discover"];
 const actions = ["your dreams", "the journey", "your power", "the light", "your potential"];
 const results = ["to change the world.", "to find your truth.", "and rise above.", "to become unstoppable.", "and shine bright."];
+let subject = subjects[Math.round(Math.random()*4)]
+let action = actions[Math.round(Math.random()*4)]
+let result = results[Math.round(Math.random()*4)]
+console.log(subject, action, result)
